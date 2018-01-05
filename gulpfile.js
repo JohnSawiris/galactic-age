a10456da8eebcc4a4c087a9d01ff4a6f153bf88c
John Sawiris <johnedwardsawiris@gmail.com>
2018-01-05 14:04:25 -0800

    add a gulp task that will concat all front end third party code and concatnate them then uglify them

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,25 @@
 var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 var browserify = require('browserify');
 var babelify = require('babelify');
 var browserSync = require('browser-sync');
 var source = require('vinyl-source-stream');
+var lib = require('bower-files')();
 
 gulp.task('jsBroswerify', function() {
   return browserify({ entries: ['./js/galactic-interface.js'] })
-  .transform(babelify.configure({
-    presets: ['es2015']
-  }))
-  .bundle()
-  .pipe(source('app.js'))
-  .pipe(gulp.dest('./build/js'));
+    .transform(babelify.configure({
+      presets: ['es2015']
+    }))
+    .bundle()
+    .pipe(source('app.js'))
+    .pipe(gulp.dest('./build/js'));
+});
+
+gulp.task('bowerJS', function() {
+  return gulp.src(lib.ext('js').files)
+    .pipe(concat(vendor.min.js))
+    .pipe(uglify())
+    .pipe(gulp.des('./build/js'));
 });
