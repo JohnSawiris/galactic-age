15a5b98bb8d1b72e6e46f3c92af8aa16f131da7a
John Sawiris <johnedwardsawiris@gmail.com>
2018-01-05 13:25:49 -0800

    add functionality to get user age in mercury and test is successful

--- js/galactic-age.js
+++ js/galactic-age.js
@@ -1,19 +1,22 @@
 export class Person {
-  constructor(birth) {
-    this.birth = birth;
+  constructor(birth, planet) {
+    this.birth = birth
+    this.planet = planet;
   }
 
-  covertAge() {
+  convertAge() {
+    //returns years difference
     let today = new Date();
     let birthDate = new Date(this.birth);
     let months =  (today.getMonth() - birthDate.getMonth()) / 12;
-    let ageDiff = parseFloat(((today.getFullYear() - birthDate.getFullYear()) + months).toFixed(1));
+    let ageDiff = Math.round(((today.getFullYear() - birthDate.getFullYear()) + months));
     let userAgeInSec = this.yearInSec() * ageDiff;
 
     return ageDiff;
   }
 
   yearInSec() {
+    //return year in seconds
     const minsInSec = 60,
      hrInSec = minsInSec * minsInSec,
      dayInSec = hrInSec* 24,
@@ -21,4 +24,32 @@ export class Person {
 
     return yrInSec;
   }
+
+  planetYear() {
+    //return planet year
+    let planetAge = 0;
+    let earthAge = this.convertAge();
+    console.log(earthAge);
+    const mercury = 1 / 0.24;
+    const venus = 1 / 0.62;
+    const mars = 1 / 1.88;
+    const jupiter = 1 / 11.86;
+
+    if(this.planet === "mercury") {
+       planetAge = (earthAge * mercury).toFixed(2);
+       return parseFloat(planetAge);
+    } else if(this.planet === "venus") {
+      planetAge = (earthAge * venus).toFixed(2);
+      return parseFloat(planetAge);
+    } else if(this.planet === "mars") {
+      planetAge = (earthAge / mars).toFixed(2);
+      return parseFloat(planetAge);
+    } else if(this.planet === "jupiter") {
+      planetAge = (earthAge / jupiter).toFixed(2);
+      return parseFloat(planetAge);
+    } else {
+      return earthAge;
+    }
+  }
+
 }
--- spec/galactic-age-spec.js
+++ spec/galactic-age-spec.js
@@ -1,8 +1,16 @@
 import { Person } from './../js/galactic-age.js';
+
 describe('Person', function() {
 
   it('should take in birthdate and return age in seconds', function() {
     let user = new Person("11-18-1989");
-    expect(user.covertAge()).toEqual(25.2);
+    expect(user.convertAge()).toEqual(28);
   });
+
+  it('should take in birthdate and planet, in this case mercury return 116.67', function() {
+    let user = new Person("11-18-1989", "mercury");
+    expect(user.planetYear()).toEqual(116.67);
+  });
+
+  
 });
